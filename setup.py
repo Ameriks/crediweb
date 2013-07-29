710125a9486b32663a0a6fae00ec1d5d361e8469
Agris Ameriks <agris.ameriks@gmail.com>
2013-07-29 18:16:41 +0300

    changed suds dependency.

--- setup.py
+++ setup.py
@@ -12,11 +12,12 @@ setup(
     packages=['crediweb'],
     description='CrediWeb parser for Python',
     url='http://github.com/Ameriks/crediweb/',
+    dependency_links = ['http://github.com/Ameriks/python-suds/tarball/master#egg=suds'],
     install_requires=[
         "requests >= 1.2.3",
         "beautifulsoup4 >= 4.2.1",
         "vatnumber >= 1.1",
-        "suds >= 0.4",
+        # "suds >= 0.4",
         "html5lib >= 1.0b2",
     ],
 )
